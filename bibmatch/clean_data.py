fbaa92a8d035f7869162bf45338fc9e174492b98
apoorva2712 <kasoju.a@husky.neu.edu>
2019-06-06 01:04:32 +0530

    changes to data cleaning and matching criteria

--- bibmatch/clean_data.py
+++ bibmatch/clean_data.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-import sys
 import string
 from nameparser import HumanName
 import re
 from unidecode import  unidecode
 import unicodedata
+from langdetect import detect 
+from dateutil.parser import parse
+import langid
 
-# from fuzzy wuzzy string processing
+# from fuzzy-wuzzy string processing
 PY3 = sys.version_info[0] == 3
 bad_chars = str("").join([chr(i) for i in range(128, 256)])  # ascii dammit!
 if PY3:
@@ -45,6 +42,63 @@ def sort_process_strings(strlist):
     else:
         return False
 
+types_of_scientists = ['\"physicist\"', r"\(physicist\)", '\"chemist\"', r"\(chemist\)", '\"biologist\"', r"\(biologist\)",
+    '\"botanist\"', r"\(botanist\)", '\"astronomer\"', r"\(astronomer\)", '\"geologist\"', r"\(geologist\)"]
+def remove_science_labels(name):
+    for label in types_of_scientists:
+        name = re.sub(label,'',name)
+    return name
+
+def strip_accents(text):
+    try:
+        text = unicode(text, 'utf-8')
+    except (TypeError, NameError): # unicode is a default on python 3
+        pass
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
+
+#to detect language of titles(articles)
+def detect_lang_and_retrieve(article):
+    #print(article)
+    match=re.search(r'\((.*?)\)',article)
+    #print(match.groups(1))
+    if match and len(match.groups())==1:
+        x=re.sub(r'\(.*\)', '', article)
+        #print(x)
+    
+        if x!='' and detect(x)!='en':
+            #print("hehe")
+            if is_date(match.group(1))==False and  detect(match.group(1))=='en':
+                return (match.group(1))
+    return article
+
+#check if string is valid date
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+    
+def clean_unicode_characters(article_title):
+    article_title=strip_accents(article_title)
+    article_title=article_title.replace("+"," ").replace("%3A"," ").replace("%2C",",").replace("%2F","/").replace("%27","'").replace("%28","(").replace("%29",")").replace("%3F","?").replace("%CF%88","ψ").replace("%E2%80%94","-").replace("%E2%80%93","-").replace("%E2%88%92","−")
+    return article_title
+
+def clean_articles(article_title):
+    article_title=re.sub('\s+', ' ', re.sub('\([\s\d\-]+\)|\d+$', '',article_title)).strip()
+    if not re.match("^[A-Z0-9/\.\-]+$",article_title) and article_title!='':
+        article_title=detect_lang_and_retrieve(article_title)
+        return article_title
 
 def remove_punctuation(personality_name):
     personality_name=personality_name.replace("-"," ")
@@ -57,9 +111,6 @@ def remove_punctuation(personality_name):
     # substitute all tabs, newlines and other "whitespace-like" characters.
     personality_name=re.sub('\s+', ' ', personality_name).strip()
     return personality_name
-
-
-
 
 
 def remove_roman_and_period(personality_name):
@@ -78,45 +129,34 @@ def remove_roman_and_period(personality_name):
     return personality_name
 
 
-types_of_scientists = ['\"physicist\"', r"\(physicist\)", '\"chemist\"', r"\(chemist\)", '\"biologist\"', r"\(biologist\)",
-    '\"botanist\"', r"\(botanist\)", '\"astronomer\"', r"\(astronomer\)", '\"geologist\"', r"\(geologist\)"]
-def remove_science_labels(name):
-    for label in types_of_scientists:
-        name = re.sub(label,'',name)
-    return name
-
-
-def strip_accents(text):
-    try:
-        text = unicode(text, 'utf-8')
-    except (TypeError, NameError): # unicode is a default on python 3
-        pass
-    text = unicodedata.normalize('NFD', text)
-    text = text.encode('ascii', 'ignore')
-    text = text.decode("utf-8")
-    return str(text)
-
-def clean_name(name):
-    if isinstance(name, str):
-        name = strip_accents(name)
-        name = clean_roman(name)
-        name = remove_science_labels(name)
-        name = name.replace('.', ' ')
-        name = name.replace("(",'"').replace(")",'"')
-        name = name.strip("\n").strip("\t").strip(" ")
+def check_name(name):
+    if ',' in name:
+        if len(str(name.split(',')[-1]))==2 or len(str(name.split(',')[-1]))==3:
+            first=name.split(',')[-1]
+            final_name=" ".join(list(first)) +" "+str(name.split(',')[0])
+        else:
+            final_name=str(name.split(',')[-1]) +" "+str(name.split(',')[0])
+        return final_name
     else:
-        name = ''
-    return HumanName(name)
-
+        return name
+    
 roman = ['I', 'V' , 'X', 'III','II','IV' ]
 def clean_roman(name):
     roman_exists= [each_roman_literal for each_roman_literal in roman if(each_roman_literal in name)]
     #based on assumption that roman letter occurs towards end on the name
     if len(roman_exists)>0:
         name=re.sub(r"\s+"+sorted(roman_exists, key=len,reverse=True)[0]+r"$",'',name)
-    return name
 
-def clean_unicode_characters(article_title):
-    article_title=strip_accents(article_title)
-    return (article_title.replace("+"," ").replace("%3A"," ").replace("%2C",",").replace("%2F","/").replace("%27","'").replace("%28","(").replace("%29",")").replace("%3F","?").replace("%CF%88","ψ").replace("%E2%80%94","-").replace("%E2%80%93","-").replace("%E2%88%92","−"))
+def clean_name(name):
+    name = strip_accents(name)
+    name = remove_roman_and_period(name)
+    name = re.sub('Jr\.$', '', name)
+    name = name.lower()
+    name = remove_science_labels(name)
+    name = name.replace('.', ' ')
+    name = name.replace("(",'"').replace(")",'"').replace("-"," ")
+    name = name.strip("\n").strip("\t").strip(" ")
+    name = re.sub('\s+', ' ', name)
+    name = check_name(name)
+    return HumanName(name)
 
--- bibmatch/fast_match_utilities.py
+++ bibmatch/fast_match_utilities.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import numpy as np
 from itertools import product
-from bibmatch.authorclass import author
+from bibmatch.clean_data import asciidammit, sort_process_strings
 from bibmatch.clean_data import asciidammit, sort_process_strings
 from Levenshtein import ratio
 
-
-def sort_ratio_similarity(s1,s2):
-    # str.lower, str.strip
-    s1, s2 = sort_process_strings([s1,s2])
-    return  ratio(s1,s2)
-
-
-
-def exact_lastname_match(a1, a2):
-    return len(a1.full_last_names.intersection(a2.full_last_names)) > 0
 
 def author_matching_vector(author1, author2):
 
@@ -27,7 +14,7 @@ def author_matching_vector(author1, author2):
 
     # 2. Does a first and middle name match?
     vector[1] = int(np.logical_or(len(author1.full_middle_names.intersection(author2.full_first_names)) > 0,
-                    len(author1.full_first_names.intersection(author2.full_middle_names)) > 0))
+                        len(author1.full_first_names.intersection(author2.full_middle_names)) > 0))
 
     # 3 Do the middle names match?
     vector[2] = int(len(author1.full_middle_names.intersection(author2.full_middle_names)) > 0)
@@ -42,9 +29,10 @@ def author_matching_vector(author1, author2):
     vector[5] = int(np.logical_or(len(author1.middle_initials.intersection(author2.first_initials)) > 0,
                     len(author1.first_initials.intersection(author2.middle_initials)) > 0))
 
-    # 7
-    vector[6] = -int(np.logical_nand(vector[0], vector[1]))
-    return vector
+   # 7 Does any of the first names in the required set not exist in first names of matching set ?
+    vector[6] = -int(1 if any(True for x in list(author2.full_first_names) if x not in list(author1.full_first_names))==True else 0)
+    
+    return (sum(vector))
 
 
 
@@ -53,61 +41,44 @@ def matching_articles(author1, author2, similarity_threshold=0.75, return_count 
     use Sorted Levenshtein distance to determine if at least one article title is similar
     '''
     count = 0
-    for a1, a2 in product(author1.processed_article_titles, author2.processed_article_titles):
-        if ratio(a1, a2) >= similarity_threshold:
-            if return_count:
-                count += 1
-            else:
-                return True
+    if len(author1.article_titles)>0 and len(author2.article_titles)>0:
+        for a1, a2 in product(author1.article_titles, author2.article_titles):
+            if ratio(a1, a2) >= similarity_threshold:
+                if return_count:
+                    count += 1
+                else:
+                    return True
     return count
 
 def matching_coauthors(author1, author2, return_count = False):
     '''
     use LastNames and the decision vector to determine if at least one co-author is similar
     '''
-    count = 0
-    for a1, a2 in product(author1.coauthor_list, author2.coauthor_list):
-        # first check the last names and then use the decision vector
-        if exact_lastname_match(a1, a2) and author_matching_vector(a1, a2).sum() >= 1:
-            if return_count:
-                count += 1
-            else:
-                return True
-    return count
+    matched_coauthors=[]
+    if len(author1.coauthor_list)>0 and len(author2.coauthor_list)>0:
+        for a1, a2 in product(author1.coauthor_list, author2.coauthor_list):
+            # first check the last names
+            if len(a1.full_last_names.intersection(a2.full_last_names)) > 0:
+                # then use the decision vector
+                if author_matching_vector(a1, a2)>= 1:
+                    if return_count:
+                        matched_coauthors.append((a1.all_names)[0])
+                    else:
+                        return True
+    return len(set(matched_coauthors))
 
 def matching_affiliations(author1, author2, similarity_threshold=0.75, return_count = False):
     '''
     use Sorted Levenshtein distance to determine if at least one affiliation is similar
     '''
-    for a1, a2 in product(author1.processed_institutions, author2.processed_institutions):
-        if ratio(a1, a2) >= similarity_threshold:
-            if return_count:
-                count += 1
-            else:
-                return True
+    count=0
+    if len(author1.institutions)>0 and len(author2.institutions)>0:
+        for a1, a2 in product(author1.institutions, author2.institutions):
+            if ratio(a1, a2) >= similarity_threshold:
+                if return_count:
+                    count += 1
+                else:
+                    return True
     return count
-
-def find_best_match(author_list1, author_list2, verbose = True):
-    # check objects are author lists and not individual authors
-    if isinstance(author_list1, author):
-        author_list1 = [author_list1]
-    if isinstance(author_list2, author):
-        author_list2 = [author_list2]
-
-    # now pick the shorter list
-    if len(author_list1) >= len(author_list2):
-        short_list, long_list = author_list2, author_list1
-    else:
-        short_list, long_list = author_list1, author_list2
-
-    matches = {}
-    for desired_author in short_list:
-        if verbose: print("Starting: ", desired_author.prefered_name)
-        # do a first pass based on last name
-        first_pass =[a for a in long_list if exact_lastname_match(a, desired_author)]
-        if verbose: print(len(first_pass), 'matches on last name')
-
-
-
 
 
